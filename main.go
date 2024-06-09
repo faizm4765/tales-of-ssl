48add94bbd66ec4edcf21d8aa59af67adf450480
faizmohammad <faizmohammad@microsoft.com>
2024-06-09 17:06:44 +0530

    added request call to endpoint

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("https://hackattic.com/challenges/tales_of_ssl/problem?access_token=b3a07ea59189199a")
+	if err != nil {
+		fmt.Printf("Error making http request: ", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Client got response!\n")
+	fmt.Printf("Status code of response: ", resp.Status)
+}
