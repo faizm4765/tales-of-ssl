8e9b0d4ac368f51726482a0bb2c8c529f313fa53
faizmohammad <faizmohammad@microsoft.com>
2024-06-09 17:15:48 +0530

    updated http to use NewRequest() method

--- main.go
+++ main.go
@@ -7,12 +7,17 @@ import (
 )
 
 func main() {
-	resp, err := http.Get("https://hackattic.com/challenges/tales_of_ssl/problem?access_token=b3a07ea59189199a")
+	req, err := http.NewRequest(http.MethodGet, "https://hackattic.com/challenges/tales_of_ssl/problem?access_token=b3a07ea59189199a", nil)
 	if err != nil {
-		fmt.Printf("Error making http request: ", err)
+		fmt.Printf("Error creating http request: ", err)
 		os.Exit(1)
 	}
 
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Printf("Error creating request: ", err)
+		os.Exit(1)
+	}
 	fmt.Printf("Client got response!\n")
 	fmt.Printf("Status code of response: ", resp.Status)
 }
